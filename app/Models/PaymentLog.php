3bd2d99768fbbedc11fc6be3bdfce0d8d56b6447
Farhan Mustaqiem <farhan.mustqm@gmail.com>
2022-08-12 11:27:54 +0700

    first commit

---
+++ app/Models/PaymentLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentLog extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+    
+    protected $casts = [
+        'created_at' => 'datetime:Y-m-d H:i:s',
+        'updated_at' => 'datetime:Y-m-d H:i:s',
+    ];
+
+}
